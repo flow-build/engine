93ff83e6d2df72cfc6cdc98089ca96a8f5c7865f
Gustavo Haramura <gustavo.haramura@fdte.io>
2021-11-29 12:53:14 -0300

    fix: change default knexenv value

--- src/core/db.js
+++ src/core/db.js
@@ -1,7 +1,8 @@
+require("dotenv").config();
 const knex = require("knex");
 const knexConfig = require("../../knexfile");
-const env = process.env.NODE_ENV || "local";
+const env = process.env.NODE_ENV || "local_docker_db";
 
-const db = knex(knexConfig[env])
+const db = knex(knexConfig[env]);
 
 module.exports = db;
