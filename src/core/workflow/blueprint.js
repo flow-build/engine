bb894c71449db0c88dc57aafe8f0fb9930ecf3cc
Gustavo Haramura <gustavo.haramura@fdte.io>
2022-02-16 08:28:08 -0300

    style: remove console.log

--- src/core/workflow/blueprint.js
+++ src/core/workflow/blueprint.js
@@ -107,7 +107,6 @@ class Blueprint {
       emitter.emit("BLUEPRINT.UNUSED_VARIABLES", "UNUSED ENVIRONMENT VARIABLES", { nodes: blueprint_env_status.nodes });
       return [true, blueprint_env_status.nodes];
     } else if (!lodash.isEmpty(blueprint_env_status.ambient)) {
-      console.log("hey!");
       emitter.emit("BLUEPRINT.NON_EXISTENT_VARIABLES", "NON EXISTENT ENVIRONMENT VARIABLES", {
         ambient: blueprint_env_status.ambient,
       });
