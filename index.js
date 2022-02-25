0853817d653ff7dcf442afd83f555a13933503e2
Gustavo Haramura <gustavo.haramura@fdte.io>
2022-02-25 16:01:08 -0300

    feat: add processStatus to index

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { Cockpit } = require("./src/cockpit/cockpit");
 
 const Nodes = require("./src/core/workflow/nodes/index.js");
 const { nodefyFunction, nodefyClass } = require("./src/core/utils/nodefy");
+const { ProcessStatus } = require("./src/core/workflow/process_state");
 
 module.exports = {
   Engine: Engine,
@@ -12,4 +13,5 @@ module.exports = {
     nodefyFunction: nodefyFunction,
     nodefyClass: nodefyClass,
   },
+  ProcessStatus,
 };
