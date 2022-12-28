28449667fdc4756cd864bbcc0a6f726ea2667855
Pedro Pereira Assis <pedro.assis@fdte.io>
2022-12-28 11:13:07 -0300

    feat: add missing httpNoSSL node

--- src/core/utils/httpNoSSL.js
+++ src/core/utils/httpNoSSL.js
@@ -1,18 +1,17 @@
 module.exports = {
-  httpNoSSLRun: async (executionData) => {
+  httpNoSSLRun: async (executionData, request) => {
     const axios = require('axios').default;
     const https = require('https');
-    const httpsAgent = new https.Agent({ rejectUnauthorized: false })
-    axios.defaults.httpsAgent = httpsAgent
+    const httpsAgent = new https.Agent({ rejectUnauthorized: false });
+    axios.defaults.httpsAgent = httpsAgent;
     
-    const { verb, url, headers } = this.request;
+    const { verb, url, headers } = request;
     const result = await axios({
         method: verb,
         data: executionData,
         url: url,
         headers: headers,
     });
-
     return result;
   }
 }
