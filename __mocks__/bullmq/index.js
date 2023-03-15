b66858d93ade8b85421f213c72c32ac201419621
Gustavo Haramura <gustavo.haramura@fdte.io>
2023-03-15 09:37:56 -0300

    test: add bullmq mock

---
+++ __mocks__/bullmq/index.js
@@ -0,0 +1,15 @@
+class Queue {
+  constructor() {}
+
+  async add(a) {
+    return {
+      name: a.name,
+      id: "b8a3990f-39ac-41b6-a0f5-971f3156521e",
+      delay: 1000,
+    };
+  }
+}
+
+module.exports = {
+  Queue,
+};
