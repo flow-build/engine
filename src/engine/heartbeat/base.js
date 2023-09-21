ab7e76d328c4a0c985106ca17656d01ee0700b7e
Pedro Pereira Assis <pedro.assis@fdte.io>
2023-09-21 15:02:18 -0300

    refactor: rearrange engineHeartBeat instances order

--- src/engine/heartbeat/base.js
+++ src/engine/heartbeat/base.js
@@ -5,10 +5,8 @@ const { processHeartBeat } = require("./process");
 const engineHeartBeat = async () => {
   emitter.emit("ENGINE.HEARTBEAT", `HEARTBEAT @ [${new Date().toISOString()}]`);
 
-  await Promise.all([
-    timerHeartBeat(),
-    processHeartBeat(),
-  ]);
+  await timerHeartBeat();
+  await processHeartBeat();
 }
 
 module.exports = {
